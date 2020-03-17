6d46cecc0e0d2b314b7125e2326b4e9e59ededc1
footerbird <footerbird@163.com>
2020-03-17 10:42:59 +0800

    控制muted

--- application/views/mobile/advert_list.php
+++ application/views/mobile/advert_list.php
@@ -20,7 +20,7 @@
                     <?php }else if($advert->ad_type == 'iframe'){ ?>
                     <iframe src="<?php echo $advert->ad_address; ?>" frameborder="0" class="banner" width="100%" height="100%"></iframe>
                     <?php }else{ ?>
-                    <video class="banner" src="<?php echo $advert->ad_address; ?>" poster="<?php echo $advert->video_poster; ?>" type="video/mp4" loop="loop" preload="load" muted webkit-playsinline="true" playsinline="true" x-webkit-airplay="true" x5-video-player-type="h5" x5-video-player-fullscreen="portraint">当前浏览器不支持</video>
+                    <video class="banner" src="<?php echo $advert->ad_address; ?>" poster="<?php echo $advert->video_poster; ?>" type="video/mp4" loop="loop" preload="load" webkit-playsinline="true" playsinline="true" x-webkit-airplay="true" x5-video-player-type="h5" x5-video-player-fullscreen="portraint">当前浏览器不支持</video>
                     <?php } ?>
                     <div class="swiper-mask">
                         <div class="share-info" style="bottom: 80px;">
